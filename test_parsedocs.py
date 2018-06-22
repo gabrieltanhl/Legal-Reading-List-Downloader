de855b41bb92a320ffc7cc595762b1c643663273
Junxuan <njunxuan@gmail.com>
2018-06-23 01:54:11 +0800

    Add additional test cases

--- test_parsedocs.py
+++ test_parsedocs.py
@@ -1,13 +1,40 @@
+import os
 import parsedocs
 
-def test_start_extract_correct_number_of_cases():
-    citation_list = parsedocs.start_extract('./test-cases/test-case-1.docx')
-    actual_citation_list = []
-    with open('./test-cases/test-output-1') as test_output:
-        for row in test_output:
-            # exclude newline char
-            actual_citation_list.append(row[:-1])
-    num_citation_list = len(set(citation_list))
-    num_actual_citation_list = len(set(actual_citation_list))
+TEST_DIR = './test-cases'
+TEST_CASE_PREFIX = 'test-case'
+TEST_OUTPUT_PREFIX = 'test-output'
+NO_OF_TESTS = 2
 
-    assert num_citation_list == num_actual_citation_list, f"Expected {num_actual_citation_list} cases, only found {num_citation_list} cases"
+def load_expected_citation_list(test_number):
+    expected_citation_list = []
+    test_output_path = os.path.join(TEST_DIR, '{}-{}'.format(TEST_OUTPUT_PREFIX, test_number))
+    with open(test_output_path) as test_output:
+        for row in test_output:
+            # remove newline char
+            row = row[:-1] if row[-1] == '\n' else row
+            expected_citation_list.append(row)
+
+    return set(expected_citation_list)
+
+
+def test_start_extract_correct_number_of_cases():
+    for test_number in range(1, NO_OF_TESTS + 1):
+        test_case_path = os.path.join(TEST_DIR, '{}-{}.docx'.format(TEST_CASE_PREFIX, test_number))
+        citation_list = parsedocs.start_extract(test_case_path)
+        expected_citation_list = load_expected_citation_list(test_number)
+
+        num_citation = len(citation_list)
+        num_expected_citation = len(expected_citation_list)
+        assert num_citation == num_expected_citation, "Expected {} cases, but found {} cases".format(
+        num_expected_citation, num_citation)
+
+
+def test_start_extract_correct_case_citations():
+    for test_number in range(1, NO_OF_TESTS + 1):
+        test_case_path = os.path.join(TEST_DIR, '{}-{}.docx'.format(TEST_CASE_PREFIX, test_number))
+        citation_list = set(parsedocs.start_extract(test_case_path))
+        expected_citation_list = load_expected_citation_list(test_number)
+        print(expected_citation_list)
+
+        assert citation_list == expected_citation_list
