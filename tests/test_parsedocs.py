7bfd6910c2f4fbe09e5b24f655f894c3ed8ef2db
Junxuan <njunxuan@gmail.com>
2018-07-10 15:55:43 +0800

    Move tests-cases into tests folder

--- tests/test_parsedocs.py
+++ tests/test_parsedocs.py
@@ -2,14 +2,14 @@ import os
 import parsedocs
 import pytest
 
-TEST_DIR = './test-cases'
+TEST_DIR = 'tests/test-cases'
 TEST_CASE_PREFIX = 'test-case'
 TEST_OUTPUT_PREFIX = 'test-output'
 NO_OF_TESTS = 2
 
 def load_expected_citation_list(expected_output_file):
     expected_citation_list = []
-    test_output_path = os.path.join(TEST_DIR, expected_output_file)
+    test_output_path = os.path.join(os.getcwd(), TEST_DIR, expected_output_file)
     with open(test_output_path) as test_output:
         for row in test_output:
             # remove newline char
@@ -24,7 +24,7 @@ def load_expected_citation_list(expected_output_file):
     ('test-case-2.docx', 'test-output-2'),
 ])
 def test_start_extract_correct_number_of_cases(test_case, expected_output):
-    test_case_path = os.path.join(TEST_DIR, test_case)
+    test_case_path = os.path.join(os.getcwd(), TEST_DIR, test_case)
     print(test_case_path)
     citation_list = parsedocs.start_extract(test_case_path)
     expected_citation_list = load_expected_citation_list(expected_output)
@@ -40,7 +40,7 @@ def test_start_extract_correct_number_of_cases(test_case, expected_output):
     ('test-case-2.docx', 'test-output-2'),
 ])
 def test_start_extract_correct_case_citations(test_case, expected_output):
-    test_case_path = os.path.join(TEST_DIR, test_case)
+    test_case_path = os.path.join(os.getcwd(), TEST_DIR, test_case)
     citation_list = set(parsedocs.start_extract(test_case_path))
     expected_citation_list = load_expected_citation_list(expected_output)
 
