df34a02f5f8db85bd0df39a920d6aedb8bd34e8c
Gabriel Tan <gabriel.tank@gmail.com>
2018-07-22 22:41:39 +0800

    updated UI
    still working on creating signals for the checkable cases

--- mainapp.py
+++ mainapp.py
@@ -45,7 +45,7 @@ class ProgressBar(QtCore.QThread):
                     self.download_status.emit(login_status)
 
                 elif login_status == 'SUCCESS':
-                    self.download_status.emit('\nLogin success!')
+                    self.download_status.emit('Login success!')
 
                     for i in self.citation_list:
                         self.progress_counter += self.progress_per_case
@@ -106,64 +106,175 @@ class ProgressBar(QtCore.QThread):
                     self.finish_job(downloader)
 
 
-class DownloaderApp(QtWidgets.QWidget):
+class App(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        self.citation_list = []
+        self.title = 'Reading List Downloader'
+        self.left = 600
+        self.top = 400
+        self.width = 800
+        self.height = 500
         self.download_directory = None
-        self.welcome_message = 'This is a tool that helps you download Singapore cases from Lawnet.\n\nInstructions:\n(1) Enter your SMU login credentials.\n\n(2) Load a reading list (only in .docx or .pdf formats).\n\n(3) Click the download button.'
+        self.citation_list = []
+        self.initUI()
 
-        self.usernamebox = QtWidgets.QLineEdit()
-        self.usernamebox.setPlaceholderText('Username e.g. johnlee.2014')
-        self.passwordbox = QtWidgets.QLineEdit()
-        self.passwordbox.setEchoMode(QtWidgets.QLineEdit.Password)
-        self.passwordbox.setPlaceholderText('Password')
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
 
+        self.createMenuBar()
+        self.createTable()
+        self.createLeftColumn()
+        self.createProgressBar()
+        self.setStyles()  # styling the menubar
+
+        # main_layout consists of button column + download table
+        # ratio of left_column width to table width is 1:3
+        self.main_layout = QtWidgets.QHBoxLayout()
+        self.main_layout.addLayout(self.left_layout, 1)
+        self.main_layout.addWidget(self.tableWidget, 3)
+
+        # overall layout which consists of the main_layout and the menu_bar
+        self.overall_layout = QtWidgets.QVBoxLayout()
+        self.overall_layout.addWidget(self.menu_bar)
+        self.overall_layout.addLayout(self.main_layout)
+        self.overall_layout.addWidget(self.progress)
+        self.status_label = QtWidgets.QLabel()
+        self.overall_layout.addWidget(self.status_label)
+
+        # add overall_layout to app
+        self.setLayout(self.overall_layout)
+
+        # hide progress bar on initial launch
+        self.progress.close()
+
+        # Show widget
+        self.show()
+
+    def create_messagebox(self):
+        print('hello')
+        # message_box = QtWidgets.QMessageBox()
+        # message_box.setText('Hello World')
+        # message_box.exec()
+
+    def createProgressBar(self):
         self.progress = QtWidgets.QProgressBar()
-        self.progress.setValue(3)
+        self.progress.setValue(1)
         self.progress.setStyle(QtWidgets.QStyleFactory.create('fusion'))
 
-        self.import_button = QtWidgets.QPushButton('Load Reading List', self)
+    def createLeftColumn(self):
+        self.usernamebox = QtWidgets.QLineEdit()
+        self.usernamebox.setPlaceholderText(' Username e.g. johnlee.2014')
+
+        self.passwordbox = QtWidgets.QLineEdit()
+        self.passwordbox.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.passwordbox.setPlaceholderText(' Password')
+
+        self.lawnet_type = QtWidgets.QComboBox()
+        self.lawnet_type.setSizeAdjustPolicy(
+            QtWidgets.QComboBox.AdjustToMinimumContentsLength)
+        self.lawnet_type.addItems(['SMU (student)', 'SMU (faculty)'])
+
+        self.start_button = QtWidgets.QPushButton('Start Download', self)
+        self.start_button.clicked.connect(self.start_download)
+
+        self.import_button = QtWidgets.QPushButton(
+            'Load Reading List', self)
         self.import_button.clicked.connect(self.showDialog)
 
         self.directory_button = QtWidgets.QPushButton(
             'Select Download Directory', self)
         self.directory_button.clicked.connect(self.select_download_directory)
 
-        self.start_button = QtWidgets.QPushButton('Start Download', self)
-        self.start_button.setDisabled(True)
-        self.usernamebox.textChanged.connect(self.disableButton)
-        self.passwordbox.textChanged.connect(self.disableButton)
-        self.start_button.clicked.connect(self.start_download)
+        self.left_layout = QtWidgets.QVBoxLayout()
+        self.left_layout.addWidget(self.usernamebox)
+        self.left_layout.addWidget(self.passwordbox)
+        self.left_layout.addWidget(self.lawnet_type)
+        self.left_layout.addStretch()
+        self.left_layout.addWidget(self.import_button)
+        self.left_layout.addWidget(self.directory_button)
+        self.left_layout.addWidget(self.start_button)
 
-        self.messagebox = QtWidgets.QTextEdit()
-        self.messagebox.setReadOnly(True)  # make it non-editable
-        self.messagebox.insertPlainText(self.welcome_message)
+    def createTable(self):
+        # Create table
+        self.tableWidget = QtWidgets.QTableWidget()
 
-        grid = QtWidgets.QGridLayout()
-        grid.setContentsMargins(15, 5, 15, 5)  # left, top, right, bottom
-        grid.setVerticalSpacing(5)
-        grid.setColumnStretch(0, 6)
-        grid.setColumnStretch(1, 4)
+        # create columns and set sizing options
+        self.tableWidget.setColumnCount(3)
+        self.tableWidget.setHorizontalHeaderLabels(
+            ['Case Title', 'Download?', 'Download Status'])
+        header = self.tableWidget.horizontalHeader()
+        header.setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
+        header.setSectionResizeMode(1, QtWidgets.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(2, QtWidgets.QHeaderView.ResizeToContents)
 
-        grid.addWidget(self.usernamebox, 0, 1)
-        grid.addWidget(self.passwordbox, 1, 1)
-        grid.addWidget(self.import_button, 2, 1)
-        grid.addWidget(self.directory_button, 3, 1)
-        grid.addWidget(self.start_button, 4, 1)
-        grid.addWidget(self.messagebox, 0, 0, 5, 1)
-        grid.addWidget(self.progress, 5, 0, 1, 2)
+        # emit signal when any checkbox is unchecked
+        # NOT WORKING CURRENTLY
+        self.tableWidget.itemChanged.connect(self.create_messagebox())
 
-        self.setLayout(grid)
+    def construct_row_from_list(self, row_num, case_title):
+        self.tableWidget.insertRow(row_num)
+        self.tableWidget.setItem(
+            row_num, 0, QtWidgets.QTableWidgetItem(case_title))
 
-        self.setGeometry(600, 400, 650, 390)
-        self.setWindowTitle('Reading List Downloader (SMU version)')
-        self.show()
-        self.progress.close()
+        checkbox = QtWidgets.QTableWidgetItem(case_title)
+        checkbox.setCheckState(QtCore.Qt.Checked)
 
-    """
-    Connectors
-    """
+        self.tableWidget.setItem(
+            row_num, 1, checkbox)
+        self.tableWidget.setItem(
+            row_num, 2, QtWidgets.QTableWidgetItem("-"))
+
+    def createMenuBar(self):
+        self.menu_bar = QtWidgets.QMenuBar(self)
+        self.menu_bar.addMenu('Instructions')
+        self.menu_bar.addMenu('About')
+        self.menu_bar.setNativeMenuBar(False)
+
+    def setStyles(self):
+        self.setStyleSheet("""
+        QMenuBar {
+            background-color: rgb(49,49,49);
+            color: rgb(255,255,255);
+            border: 1px solid #000;
+        }
+
+        QMenuBar::item {
+            background-color: rgb(49,49,49);
+            color: rgb(255,255,255);
+        }
+
+        QMenuBar::item::selected {
+            background-color: rgb(30,30,30);
+        }
+
+        QMenu {
+            background-color: rgb(49,49,49);
+            color: rgb(255,255,255);
+            border: 1px solid #000;
+        }
+
+        QMenu::item::selected {
+            background-color: rgb(30,30,30);
+        }
+    """)
+
+    @Slot()
+    def showDialog(self):
+        fname = QtWidgets.QFileDialog.getOpenFileName(self, 'Open file',
+                                                      '/home')
+        # with the case names, construct the table
+        if fname[0]:
+            self.citation_list = parsedocs.start_extract(fname[0])
+            for row_num, case_title in enumerate(self.citation_list):
+                self.construct_row_from_list(row_num, case_title)
+
+    @Slot()
+    def on_click(self):
+        print("\n")
+        for currentQTableWidgetItem in self.tableWidget.selectedItems():
+            print(currentQTableWidgetItem.row(),
+                  currentQTableWidgetItem.column(), currentQTableWidgetItem.text())
 
     @Slot()
     def disableButton(self):
@@ -172,17 +283,6 @@ class DownloaderApp(QtWidgets.QWidget):
             self.start_button.setDisabled(False)
         else:
             self.start_button.setDisabled(True)
-
-    @Slot()
-    def showDialog(self):
-        fname = QtWidgets.QFileDialog.getOpenFileName(self, 'Open file',
-                                                      '/home')
-        if fname[0]:
-            self.citation_list = parsedocs.start_extract(fname[0])
-            self.messagebox.insertPlainText(
-                '\n\n' + str(len(self.citation_list)) +
-                ' cases which can be found on LawNet:\n' +
-                '\n'.join(self.citation_list))
 
     @Slot()
     def select_download_directory(self):
@@ -205,11 +305,11 @@ class DownloaderApp(QtWidgets.QWidget):
             self.calc.download_status.connect(self.update_download_status)
             self.start_button.setDisabled(True)
             self.progress.show()
-            self.messagebox.clear()
-            self.messagebox.insertPlainText('Logging in...')
+            self.status_label.clear()
+            self.status_label.setText('Logging in...')
         else:
-            self.messagebox.clear()
-            self.messagebox.insertPlainText(
+            self.status_label.clear()
+            self.status_label.setText(
                 'No cases detected. Please load a reading list.')
 
     def update_progress_bar(self, progress_counter):
@@ -218,27 +318,21 @@ class DownloaderApp(QtWidgets.QWidget):
         if progress_counter == 100 and self.progress.value != 5:
             self.start_button.setDisabled(False)
             self.progress.close()
-            self.progress.setValue(5)
-            self.messagebox.insertPlainText('\nDownload complete!')
+            self.progress.setValue(1)
+            self.status_label.setText('Download complete!')
 
     def update_download_status(self, download_status):
         if download_status == 'FAIL':
             self.start_button.setDisabled(False)
             self.progress.close()
-            self.progress.setValue(5)
-            self.messagebox.clear()
-            self.messagebox.insertPlainText(
-                'Login failed, please try again.\n\n')
-            self.messagebox.insertPlainText(self.welcome_message)
+            self.progress.setValue(1)
+            self.status_label.setText(
+                'Login failed, please try again.')
         else:
-            self.messagebox.insertPlainText(download_status)
+            self.status_label.setText(download_status)
 
 
-if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
-    app.setApplicationDisplayName('Reading List Downloader')
-    app.setWindowIcon(QtGui.QIcon('icon.icns'))
-    widget = DownloaderApp()
-    widget.show()
-
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    ex = App()
     sys.exit(app.exec_())
