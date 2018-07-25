8ddba1f0125715606edfef9b66f019c4c5f61fdf
Wan Ding Yao <DYWAN@live.com.sg>
2018-07-25 11:07:36 +0800

    Add support for Chrome integration

--- virtualbrowser.py
+++ virtualbrowser.py
@@ -8,7 +8,7 @@ if getattr(sys, 'frozen', False):
 else:
     application_path = os.path.dirname(os.path.abspath(__file__))
 
-CHROMEPATH = str(application_path)+'/chromedriver'
+CHROMEPATH = str(application_path)+'/chromedriver.exe'
 
 
 def chrome():
@@ -17,6 +17,7 @@ def chrome():
     chrome_options.add_argument("--screen-size=1920x1080")
     chrome_options.add_argument(
         "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/604.5.6 (KHTML, like Gecko) Version/11.0.3 Safari/604.5.6")
+    chrome_options.binary_location = str(application_path)+'/chrome.exe'
     chrome_options.add_argument("--disable-extensions")
     driver = webdriver.Chrome(
         executable_path=CHROMEPATH, chrome_options=chrome_options)
