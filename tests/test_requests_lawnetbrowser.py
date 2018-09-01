c81025284d5d9e4743805423d3e78fb0d22bad01
Junxuan <njunxuan@gmail.com>
2018-09-02 03:02:55 +0800

    Refactor download test and add more test cases

--- tests/test_requests_lawnetbrowser.py
+++ tests/test_requests_lawnetbrowser.py
@@ -2,30 +2,22 @@ from lawnetsearch import LawnetBrowser
 from credentials import login, false_login
 from sha_helpers import generate_sha_hash_helper, read_sha_from_file_helper
 import pytest
-import os
-import threading
+from pathlib import Path
 
-BASE_DIR = os.getcwd()
-SHA_DIR = os.path.join(BASE_DIR, 'tests/sha/')
-DOWNLOAD_DIR = os.path.join(BASE_DIR, 'tests/downloads/')
+BASE_DIR = Path.cwd()
+SHA_DIR = Path(BASE_DIR) / 'tests/sha/'
+DOWNLOAD_DIR = Path(BASE_DIR) / 'tests/downloads/'
 
 
 @pytest.fixture(scope='class')
 def browser(request):
-
     browser = LawnetBrowser()
-
     request.cls.browser = browser
     yield
 
 
 @pytest.mark.usefixtures('browser')
 class TestRequestsBrowser:
-    # @classmethod
-    # def setup_class(self):
-    #     self.browser = LawnetBrowser(
-    #         login['username'], login['password'], DOWNLOAD_DIR)
-
     def test_incorrect_login(self):
         self.browser.update_download_info(false_login['username'],
                                           false_login['password'], 'smustu',
@@ -41,24 +33,24 @@ class TestRequestsBrowser:
 
     @pytest.mark.vcr()
     @pytest.mark.parametrize(
-        'test_title, test_citation, expected_ext, expected_sha_file',
+        'test_citation, expected_title',
         [
-            ('Living the Link Pte Ltd (in creditors voluntary liquidation) and others v Tan Lay Tin Tina and others', '[2016] 3 SLR 621', 'pdf', '[2016] 3 SLR 621.sha'),
-            ('Stonegate Securities Ltd. V. Gregory [1979 S. No. 053]', '[1980] 1 Ch. 576', 'pdf', '[1980] 1 Ch 576.sha'),
+            ('[2016] 3 SLR 621', 'Living the Link Pte Ltd (in creditors voluntary liquidation) and others v Tan Lay Tin Tina and others - [2016] 3 SLR 621'),
+            ('[1980] 1 Ch. 576', 'Stonegate Securities Ltd. V. Gregory [1979 S. No. 053] - [1980] 1 Ch. 576'),
+            ('[1980] 1 Ch 576', 'Stonegate Securities Ltd. V. Gregory [1979 S. No. 053] - [1980] 1 Ch. 576'),
             # ('[1989] 3 MLJ 385', 'html', '[1989] 3 MLJ 385.sha'),
-            ('In Re Atlantic Computer Systems Plc.', '[1992] 2 WLR 367', 'pdf', '[1992] 2 WLR 367.sha')
+            ('[1992] 2 WLR 367', 'In Re Atlantic Computer Systems Plc. - [1992] 2 WLR 367'),
+            ('[1951] 1 A.C. 850', 'Bolton and Others Appellants and Stone Respondent. - [1951] 1 A.C. 850')
         ])
-    def test_case_download(self, test_title, test_citation, expected_ext,
-                           expected_sha_file):
-        search_lock = threading.Lock()
-        self.browser.download_case(test_citation, search_lock)
-        downloaded_case_name = f'{test_title} - {test_citation}.{expected_ext}'
-        downloaded_case_path = os.path.join(DOWNLOAD_DIR, downloaded_case_name)
-        sha_file_path = os.path.join(SHA_DIR, expected_sha_file)
+    def test_successful_case_download(self, test_citation, expected_title):
+        self.browser.download_case(test_citation)
+        downloaded_case_name = f'{expected_title}.pdf'
+        downloaded_case_path = Path(DOWNLOAD_DIR) / downloaded_case_name
+        sha_file_path = Path(SHA_DIR) / f'{test_citation}.sha'
         # assert file downloaded with correct extension
-        assert downloaded_case_name in os.listdir(DOWNLOAD_DIR)
+        assert Path(downloaded_case_path).is_file()
 
         # assert file is correct
         assert generate_sha_hash_helper(
             downloaded_case_path) == read_sha_from_file_helper(sha_file_path)
-        os.remove(downloaded_case_path)
+        Path.unlink(downloaded_case_path)
